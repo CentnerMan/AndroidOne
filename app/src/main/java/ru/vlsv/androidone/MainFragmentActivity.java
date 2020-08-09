1268176fbcd6b454a05da557df6470d78c71093c
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-08-09 22:29:16 +0300

    hw-5-first

--- app/src/main/java/ru/vlsv/androidone/MainFragmentActivity.java
+++ app/src/main/java/ru/vlsv/androidone/MainFragmentActivity.java
@@ -1,6 +1,7 @@
 package ru.vlsv.androidone;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainFragmentActivity extends AppCompatActivity {
@@ -9,5 +10,16 @@ public class MainFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int countOfFragmentInManager = getSupportFragmentManager().getBackStackEntryCount();
+        if(countOfFragmentInManager > 0) {
+            getSupportFragmentManager().popBackStack();
+            /*getSupportFragmentManager().popBackStack("Some_Key",
+                    FragmentManager.POP_BACK_STACK_INCLUSIVE);*/
+        }
     }
 }
